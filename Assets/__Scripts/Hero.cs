a9e45faf0db76509ad4d7b35803805d42710119f
SangHLee246 <akayasha1234@yahoo.com>
2016-06-20 19:44:32 -0400

    Added More Prefabs
    Weapons + Projectiles + Fixed errors

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -5,13 +5,16 @@ public class Hero : MonoBehaviour {
 
 	static public Hero		S; //Singleton
 
+	public float gameRestartDelay = 2f;
+	
 	//These fields control the movement of the ship
 	public float	speed = 30;
 	public float	rollMult = -45;
 	public float  	pitchMult=30;
 
 	//Ship status information
-	public float	shieldLevel=1;
+	[SerializeField]
+	public float	_shieldLevel=1;
 
 	public bool	_____________________;
 	public Bounds bounds;
@@ -77,6 +80,21 @@ public class Hero : MonoBehaviour {
 			print("Triggered: "+go.name);
 			//Ohterwise announce the original other.gameObject
 			print("Triggered: "+other.gameObject.name);
+			}
+		}
+	}
+
+	public float shieldLevel {
+		get {
+			return( _shieldLevel);
+		}
+		set {
+			_shieldLevel = Mathf.Min(value, 4);
+			//If the shield is going to be set to less than zero
+			if (value < 0) {
+				Destroy(this.gameObject);
+			//Tell Main.S to restart the game after a delay
+				Main.S.DelayedRestart(gameRestartDelay);
 			}
 		}
 	}
